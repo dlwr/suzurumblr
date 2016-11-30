1969bc2a61ff35dc1a318762cc55e3e3e87222e6
yuta25 <kawai@pepabo.com>
2016-11-30 11:35:26 +0900

    omoide

--- app.js
+++ app.js
@@ -7,8 +7,15 @@ const client = tumblr.createClient({
   returnPromises: true
 })
 const cronJob = require('cron').CronJob
+const fs = require('fs')
 
 const SUZURI_TOKEN = 'TOKEN'
+const SUZURI_API_BASE_URL = 'https://suzuri.jp/api/v1'
+const SUZURI_AUTH_HEADER = {
+  'Content-Type': 'application/json',
+  'Authorization': `Bearer ${SUZURI_TOKEN}`
+}
+const ACCOUNT = 'reblogadventcalender7'
 const itemVariants = {"1": [71, 99, 431, 89, 103, 389, 86, 72, 5, 49, 371, 1, 7, 8, 11, 2, 3, 4, 6, 9, 10, 12, 16, 17, 18, 19, 20, 21, 27, 28, 31, 22, 23,24, 25, 26, 29, 30, 32, 33, 36, 37, 39, 40, 41, 47, 51, 42, 43, 44, 45, 46, 50, 52, 53, 54, 56, 57, 59, 60, 61, 67, 68, 62, 63, 64, 69, 70, 65, 66, 48, 381, 14, 74, 38, 58, 34, 77, 79, 80, 90, 94, 88, 93, 95, 96, 98, 100, 101, 104, 117, 127, 317, 321, 124, 78, 73, 87, 102, 91, 92, 107, 84, 85, 97, 106, 105, 156, 449, 164, 167, 441, 355, 407, 409, 410, 411, 412, 413, 417, 419, 420, 421, 422, 423, 424, 425, 427, 428, 152, 166, 429, 430, 435, 437, 438, 439, 440, 442, 443, 445, 446, 447, 448, 453, 456, 165, 162, 159, 154, 163, 160, 153, 415, 433, 335, 386, 405, 404, 343, 353, 457, 458, 460, 451, 459, 455, 13, 350, 76, 368, 339, 357, 323, 393, 320, 338, 356, 392, 351, 369, 387, 347, 365, 383, 401, 359, 395, 348, 366, 384, 402, 399, 329, 330, 327, 332, 333, 377, 374, 341, 375, 345, 340, 358, 376, 394, 349, 367, 385, 403, 352, 370, 388, 406, 337, 373, 361, 379, 134, 391, 331, 334, 151, 118, 325, 363, 158, 319, 135, 144, 322, 397, 161, 132, 149, 131, 148, 128, 145, 125, 142, 120, 137, 129, 146, 126, 143, 119, 136, 122, 139, 141, 130, 147, 133, 150],
                       "8": [602, 461, 462, 463],
                       "5": [174, 228, 175, 203, 210, 188, 242, 185, 237, 194, 200, 251, 215, 275, 218, 222, 227, 230, 183, 198, 176, 209, 238, 239, 240, 244, 245, 246, 252, 253, 254, 255, 256, 257, 259, 264, 266, 267, 268, 269, 270, 271, 272, 274, 276, 233, 265, 173, 273, 190, 177, 250, 192, 234, 261, 207, 195, 205, 220, 191, 189, 204, 219, 213, 263, 249, 260, 241, 211, 236, 248, 221, 206, 235, 197, 258, 225, 243, 182,184, 186, 196, 193, 199, 201, 208, 212, 214, 216, 223, 178, 224, 231, 226, 229, 179, 180, 181],
@@ -20,13 +27,34 @@ const itemVariants = {"1": [71, 99, 431, 89, 103, 389, 86, 72, 5, 49, 371, 1, 7,
                       "6": [286, 287, 289, 288, 278, 464, 293, 295, 296, 297, 298, 300, 301, 302, 303, 299, 279, 281, 282, 283, 284, 285],
                       "7": [308, 307, 314, 309, 311, 316, 310, 313, 312, 315],
                       "12": [607],
-                      "11": [606]
+                      "11": [606],
+                      "13": [610]
                      }
-let lastPostId = 153253470873
+let lastPostId
+let lastOmoide
 function poll() {
-  var last = 0
-  client.blogPosts('dlwr', {api_key: 'KEY'})
-    .then(resp => {
+  new Promise((resolve, reject) => {
+    return fs.readFile('./lastPostId', function (err, text) {
+      if (err) {
+        lastPostId = 0
+      } else {
+        lastPostId = parseInt(text)
+      }
+      return resolve()
+    })
+  }).then(() => {
+    return fetch(`${SUZURI_API_BASE_URL}/choices?userName=${ACCOUNT}`, {
+      headers: SUZURI_AUTH_HEADER
+    })
+  }).then(resp => resp.json())
+  .then(json => {
+    if (json.choices && json.choices.length) {
+      lastOmoide = json.choices[0]
+    }
+    return
+  }).then(() => {
+    return client.blogPosts(ACCOUNT, {api_key: 'KEY'})
+  }).then(resp => {
       const promises = []
       const posts = resp.posts.sort((a, b) => {
         if (a.id < b.id) return -1
@@ -39,6 +67,7 @@ function poll() {
           continue
         }
         lastPostId = post.id
+      let lastPostDate = (new Date(post.timestamp * 1000)).toDateString()
         let res
         if (post.type == 'photo') {
           let itemId = Math.floor(Math.random() * Object.keys(itemVariants).length) + 1
@@ -46,6 +75,7 @@ function poll() {
           let targetVariants = itemVariants[itemId]
           let itemVariantId = targetVariants[Math.floor(Math.random() * targetVariants.length)]
           let title = post.summary ? post.summary : (post.slug ? post.slug : post.post_url)
+        console.log(post.id + 'の画像を同期します')
           res = fetch('https://suzuri.jp/api/v1/materials', {
             method: 'POST',
             headers: {
@@ -79,11 +109,38 @@ function poll() {
             })
           })
         }
+      res.then(resp =>  resp.json())
+      .then(productResp => {
+        let choicePromise
+        if (!lastOmoide || lastPostDate != lastOmoide.title) {
+          choicePromise = fetch(`${SUZURI_API_BASE_URL}/choices`, {
+            method: 'POST',
+            headers: SUZURI_AUTH_HEADER,
+            body: JSON.stringify({
+              title: lastPostDate
+            })
+          }).then(res => res.json())
+        } else {
+          choicePromise = Promise.resolve(lastOmoide)
+        }
+        return choicePromise.then(omoideResp => {
+          lastOmoide = omoideResp.choice || omoideResp
+          return fetch(`${SUZURI_API_BASE_URL}/choices/${lastOmoide.id}`, {
+            method: 'POST',
+            headers: SUZURI_AUTH_HEADER,
+            body: JSON.stringify({
+              productId: productResp.products[0].id,
+              itemVariantId: productResp.products[0].sampleItemVariant.id
+            })
+          })
+        })
+      })
         promises.push(res)
       }
       return Promise.all(promises)
     }).then(values => {
       console.log(new Date(Date.now()).toISOString() + ':' + values.length + '個作りました')
+    fs.writeFile("./lastPostId", `${lastPostId}`)
     })
 }
 
